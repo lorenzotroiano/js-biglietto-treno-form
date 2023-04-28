3158543e6f9e1f60f56afd2242db86421cc712d5
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-28 16:02:51 +0200

    inseriti calcoli e le varie opzioni

--- js/trainform.js
+++ js/trainform.js
@@ -1,20 +1,76 @@
+// regole generali
+let userName, userKm, userAge, prezzoKm, percentualeSconto;
+
+
+
+
+
 // Chiedere nome e cognome all'user
 
+  function myFunction() {
+    
+    // Chiedere nome e cognome all'user
+    userName = document.getElementById("username").value;
+    
+    // chiedere km che vuole percorrere
+    userKm = document.getElementById("km").value;
+   
+    // chiedere età
+    userAge = document.getElementById("age").value;
 
 
-// chiedere km che vuole percorrere
+
+    
+    submitOK = "true";
+  
+
+    if (userAge <= 17) {
+        // minorenni 20% sconto
+        percentualeSconto = 20;
+    } else if (userAge >= 65) {
+        // over 65 sconto 40%
+        percentualeSconto = 40;
+    } else {
+        percentualeSconto = 0;
+    }
+  
+   
+  
+    if (submitOK == "false") {
+      return false;
+    }
+  }
 
 
-// chiedere età
 
+ 
 
 // prezzo km 0,21cent
+prezzoKm = parseInt(0,21);
+
+prezzoBase = prezzoKm * userKm;
+console.log(prezzoBase);
 
 
-// minorenni 20% sconto
+// minorenne = document.getElementById("minorenne").value;
+// anziano = document.getElementById("anziano").value;
+// adulto = document.getElementById("adulto").value;
 
 
-// over 65 sconto 40%
+
+
+// calcolare prezzo biglietto scontato
+prezzoSconto = userKm * percentualeSconto / 100;
+prezzoFinale = prezzoBase - prezzoSconto;
+
+console.log(prezzoSconto, prezzoFinale);
+
+
+
+
+
+document.getElementById("stampanome").innerHTML = userName;
+
 
 
 
