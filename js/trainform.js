bc54e4fcc7bf641f299c77bdecfebdc7cd07abb9
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-28 14:44:56 +0200

    creazione form

---
+++ js/trainform.js
@@ -0,0 +1,20 @@
+// Chiedere nome e cognome all'user
+
+
+
+// chiedere km che vuole percorrere
+
+
+// chiedere età
+
+
+// prezzo km 0,21cent
+
+
+// minorenni 20% sconto
+
+
+// over 65 sconto 40%
+
+
+
